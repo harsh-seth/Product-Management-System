f3ff534963ef6b41f717d373fb7243000fb56a05
Harsh Seth <harshseth2006@gmail.com>
2019-03-28 00:04:54 +0530

    Implemented and Documented Category Manipulation APIs

--- server.js
+++ server.js
@@ -10,15 +10,26 @@ app.use(express.urlencoded({extended: true}))
 const portNum = 3000
 
 const validators = {
-    
+    'category': {
+        'categoryID': joi.number().min(0).required(),
+        'name': joi.string().default(null),
+        'parentCategoryID': joi.number().min(-1).default(null)
+    } 
 }
 
 const messages = {
-    'invalidURL': "You tried to access an invalid endpoint!"
+    'invalidURL': "You tried to access an invalid endpoint!",
+    'invalidParameters': "Some parameters passed with the request are missing or invalid!",
+    'invalidCatID': "No category exists with that ID!",
+    'invalidParentCatID': "No parent category exists with that ID!",
+    'invalidProdID': "No product exists with that ID!",
+    'duplicateID': "An item with that ID already exists!",
+    'opOK': 'The operation was successful!',
 }
 
 const listOfEndpoints = [
-    '/', '/swagger'
+    '/', '/swagger',
+    '/category', '/category/{id}'
 ]
 
 var taxBrackets = {
@@ -33,16 +44,19 @@ var taxBrackets = {
 
 var categories = {
     1: {
+        categoryID: 1,
         name: "Stationary",
-        childrenCategoryIds: [2],
+        parentCategoryID: null
     },
     2: {
+        categoryID: 2,
         name: "Writing Tools",
-        childrenCategoryIds: []
+        parentCategoryID: 1
     },
     3: {
+        categoryID: 3,
         name: "Paper",
-        childrenCategoryIds: []
+        parentCategoryID: 1
     }
 }
 
@@ -50,13 +64,13 @@ var products = {
     1: {
         productSKU: 1,
         name: "Ballpoint Pen",
-        categoryId: 2,
+        categoryID: 2,
         keywords: ['pen', 'writing'],
         brand: 'Qazwer Corp.',
         color: 'blue',
         modeOfSale: 'both',
         basePrice: 2,
-        taxCategoryId: 1,
+        taxCategoryID: 1,
         imageURLs: [],
         stock: 2000,
         status: 'live'
@@ -64,13 +78,13 @@ var products = {
     2: {
         productSKU: 2,
         name: "Eraser",
-        categoryId: 1,
+        categoryID: 1,
         keywords: ['rubber'],
         brand: 'Qazwer Corp.',
         color: 'white',
         modeOfSale: 'offline',
         basePrice: 1,
-        taxCategoryId: 0,
+        taxCategoryID: 0,
         imageURLs: [],
         stock: 500,
         status: 'live'
@@ -86,12 +100,223 @@ var discounts = {
     }
 }
 
+var orders = [
+    {
+        'status': 'placed',
+        'items': {
+
+            '2': 300,
+            '1': 50
+        }
+    },
+    {
+        'status': 'placed',
+        'items': {
+            '1': 150
+        }
+    }
+]
+
+const getChildren = async function(parentID) {
+    var children = []
+    for(var category in categories) {
+        if(categories[category]['parentCategoryID'] == parentID)
+            children = children.concat(category)
+    }
+    return children
+}
+
+const getDecendants = async function(parentID) {
+    const children = await getChildren(parentID)
+    var decendants = children
+    if (children) {
+        for (var i=0; i<children.length; i++) {
+            const result = await getDecendants(children[i])
+            if (result) 
+                decendants = decendants.concat(result)
+        }
+    }
+    return decendants
+}
+
 app.get('/', (req, res) => {
+    // Send a list of all valid endpoints
     res.send({'listOfEndpoints': listOfEndpoints})
 })
 
 app.get('/swagger', (req, res) => {
+    // Serve the swagger spec file
     res.sendFile('swagger-spec.yaml', {root: __dirname})
+})
+
+app.get('/category/:categoryID', (req, res) => {
+    // Get the details of a category
+    var categoryID = req.params.categoryID
+    if (categoryID) {
+        // If valid parameters were passed
+        if (categoryID in categories) {
+            // If category exists send details
+            res.send(categories[categoryID])
+        } else {
+            // Send resource not found
+            res.status(404).send({
+                'message': messages['invalidCatID'],
+                'status': 'invalidCatID'
+            })
+        }
+    } else {
+        // Send invalid parameters
+        res.status(400).send({
+            'message': messages['invalidParameters'],
+            'status': 'invalidParameters'
+        })
+    }
+})
+
+app.post('/category', (req, res) => {
+    // Add a new category
+    // Validate input provided 
+    const result = joi.validate(req.body, validators['category'])
+    
+    if (result.error) {
+        // If validation fails
+        res.status(400).send({
+            'message': result.error.details[0].message,
+            'status': 'invalidParameters'
+        })
+    } else {
+        if (result.value.name == null) {
+            res.status(400).send({
+                'message': messages['invalidParameters'],
+                'status': 'invalidParameters'
+            })
+        } else if (result.value.categoryID in categories) {
+            // If category exists with that ID
+            res.status(400).send({
+                'message': messages['duplicateID'],
+                'status': 'duplicateID'
+            })
+        } else {
+            if (result.value.parentCategoryID != null) {
+                // If a parent category ID was provided
+                if (!(result.value.parentCategoryID in categories)) {
+                    // If parent category ID provided does not exist
+                    return res.status(400).send({
+                        'message': messages['invalidParentCatID'],
+                        'status': 'invalidParentCatID'
+                    })
+                }
+            }
+            // Everything checks out, add the category
+            categories[result.value.categoryID] = {
+                'categoryID': result.value.categoryID,
+                'name': result.value.name,
+                'parentCategoryID': result.value.parentCategoryID
+            }
+
+            res.send({
+                'message': messages['opOK'],
+                'status': 'opOK'
+            })
+        }
+    }
+})
+
+app.put('/category', (req, res) => {
+    // Update a category
+    // Validate input provided 
+    const result = joi.validate(req.body, validators['category'])
+    
+    if (result.error) {
+        // If validation fails
+        res.status(400).send({
+            'message': result.error.details[0].message,
+            'status': 'invalidParameters'
+        })
+    } else {
+        if (result.value.categoryID in categories) {
+            // If the category ID exists
+            if (result.value.parentCategoryID != null) {
+                // If a parent category ID was provided
+                if (result.value.parentCategoryID !== -1 && !(result.value.parentCategoryID in categories)) {
+                    // If parent category ID provided does not exist
+                    return res.status(400).send({
+                        'message': messages['invalidParentCatID'],
+                        'status': 'invalidParentCatID'
+                    })
+                }
+            }
+            // Everything checks out, update the category
+            for(var key in req.body) {
+                if (key === 'parentCategoryID') {
+                    if (req.body[key] === -1) {
+                        // To remove parent category from category information
+                        categories[req.body.categoryID][key] = null
+                        continue
+                    }
+                }
+                // Change the passed fields
+                categories[req.body.categoryID][key] = req.body[key]
+            }
+ 
+            res.send({
+                'message': messages['opOK'],
+                'status': 'opOK'
+            })
+        } else {
+            // If category with category ID doesn't exist
+            res.status(404).send({
+                'message': messages['invalidCatID'],
+                'status': 'invalidCatID'
+            })
+            
+        }
+    }
+})
+
+app.delete('/category/:categoryID', (req, res) => {
+    // Get the details of a category
+    var categoryID = req.params.categoryID
+    if (categoryID) {
+        // If valid parameters were passed
+        if (categoryID in categories) {
+            // If category exists, get all decendants of the category,
+            // delete categories and associated products          
+            getDecendants(categoryID)
+            .then(decendants => {return decendants.concat(categoryID)})
+            .then(toDelete => {
+                for (var prodID in products) {
+                    if (products[prodID]['categoryID'] in toDelete) {
+                        delete products[prodID]
+                    }
+                }
+
+                for (var i=0; i<toDelete.length; i++) {
+                    delete categories[toDelete[i]]
+                }
+
+                return toDelete
+            })
+            .then(deleted => {
+                res.send({
+                    'message': messages['opOK'],
+                    'status': 'opOK'
+                })
+            })
+        } else {
+            // Send resource not found
+            res.status(404).send({
+                'message': messages['invalidCatID'],
+                'status': 'invalidCatID'
+            })
+        }
+    } else {
+        // Send invalid parameters
+        res.status(400).send({
+            'message': messages['invalidParameters'],
+            'status': 'invalidParameters'
+        })
+    }
 })
 
 app.get('*', (req, res) => {
